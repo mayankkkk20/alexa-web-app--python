091d75dee2489d983e581f82d0d2bda4969b42ad
mayank Kumar <139457115+mayankkkk20@users.noreply.github.com>
2024-09-27 20:10:48 +0530

    Create alexa.py

---
+++ alexa.py
@@ -0,0 +1,62 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %P')
+        talk('current time is ' + time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif ' Can we go on a date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('No I am in a relationship with the wifi')
+    elif 'do you have feelings for humans' in command:
+        talk('no I dont have feelings for the humans')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('please say the command again')
+
+
+while True:
+    run_alexa()
